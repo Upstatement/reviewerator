be1ff9cf8b962d0aaf9e858e2965dc52874db504
Alec Marcus <alec@alecmarc.us>
2019-10-25 14:17:23 -0400

    Remove Detail, master, replace with chart

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
-import { Detail, Master } from 'components';
-import { className } from 'utils';
+import React from 'react';
+import { Chart } from 'components';
 import styles from './layout.module.scss';
 
 const Layout = () => {
-  const [masterIsHidden, setMasterIsHidden] = useState(false);
-
-  const toggleMaster = () => {
-    setMasterIsHidden(!masterIsHidden);
-  };
-
   return (
     <main className={styles.main}>
-      <button
-        onClick={toggleMaster}
-        {...className(styles.showHideBtn, masterIsHidden && styles.show)}>
-        {masterIsHidden ? 'Show Controls' : 'Hide Controls'}
-      </button>
-      <div className={styles.layout}>
-        <Master {...className(styles.master, masterIsHidden && styles.hidden)} />
-        <Detail className={styles.detail} />
-      </div>
+      <Chart />
     </main>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,5 @@ export { default as Button } from './Button';
 export { default as Chart } from './Chart';
 export { default as Layout } from './Layout';
 export { default as Link } from './Link';
-export { default as Master } from './Master';
 export { ViewContext, ViewProvider } from './viewContext';
 export { Radio, Select, Slider, SkillLevelRadio } from './Controls';

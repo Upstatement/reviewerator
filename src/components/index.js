8aa4f78d7ddba4b1604d69b11e418c5031cee566
Alec Marcus <alec@alecmarc.us>
2019-10-25 14:16:57 -0400

    Rename detail to chart, use SkillLevelRadio for inputs

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Button } from './Button';
-export { default as Detail } from './Detail';
+export { default as Chart } from './Chart';
 export { default as Layout } from './Layout';
 export { default as Link } from './Link';
 export { default as Master } from './Master';

fdc9d5aec11dc802473878ec4f551ecca9481894
Alec Marcus <alec@alecmarc.us>
2019-12-20 13:35:17 -0500

    add ability to toggle level targets

--- src/components/SkillLevelRadio/index.js
+++ src/components/SkillLevelRadio/index.js
@@ -3,11 +3,16 @@ import PropTypes from 'prop-types';
 import { className } from 'utils';
 import styles from './skillLevelRadio.module.scss';
 
-const Radio = ({ defaultVal, labelText, options, property, reportValue, }) => {
+const Radio = ({ defaultVal, labelText, options, property, reportValue }) => {
   const [value, setValue] = useState(defaultVal);
+  const [isTarget, setIsTarget] = useState(false);
 
   const handleChange = e => {
     setValue(parseInt(e.target.value));
+  };
+
+  const handleClick = isChecked => {
+    setIsTarget(isChecked && !isTarget);
   };
 
   useEffect(() => {
@@ -17,13 +22,14 @@ const Radio = ({ defaultVal, labelText, options, property, reportValue, }) => {
 
   return (
     <div className={styles.levelBar}>
-      {options.map(({ optValue, valueText, }, i) => (
+      {options.map(({ optValue, valueText }, i) => (
         <div
           key={i}
           {...className(
             styles.levelBlock,
             optValue === value && styles.currentLevel,
-            optValue <= value && styles.achieved
+            optValue <= value && styles.achieved,
+            isTarget && optValue === value + 1 && styles.target,
           )}>
           <label htmlFor={`${property}-${valueText}`} className={styles.levelBlockLabel}>
             <input
@@ -32,6 +38,7 @@ const Radio = ({ defaultVal, labelText, options, property, reportValue, }) => {
               id={`${property}-${valueText}`}
               name={property}
               onChange={handleChange}
+              onClick={() => handleClick(value === optValue)}
               type="radio"
               value={optValue}
             />
@@ -51,7 +58,7 @@ Radio.propTypes = {
       displayText: PropTypes.string.isRequired,
       optValue: PropTypes.any.isRequired,
       valueText: PropTypes.string.isRequired,
-    })
+    }),
   ),
   property: PropTypes.string.isRequired,
   reportValue: PropTypes.func.isRequired,
